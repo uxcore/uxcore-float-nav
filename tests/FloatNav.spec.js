01fbfc0c299b3c8351d4c72edfec8a1cb17b933b
surfacew <surfacew@163.com>
2018-01-02 11:00:55 +0800

    test: add test utils

--- tests/FloatNav.spec.js
+++ tests/FloatNav.spec.js
@@ -28,10 +28,15 @@ describe('FloatNav', () => {
 
   describe('More Tests', () => {
     let component;
+    let container;
     beforeEach(() => {
-      const container = document.createElement('div');
+      container = document.createElement('div');
       document.body.appendChild(container);
       ReactDOM.render(<FloatNavDemo ref={d => (component = d)} />, container);
+    });
+
+    afterEach(() => {
+      ReactDOM.unmountComponentAtNode(container);
     });
 
     it('should scroll correctly when click the control btn', (done) => {
@@ -39,6 +44,28 @@ describe('FloatNav', () => {
       Simulate.click(nextBtn);
       console.log(component.nav.state.scrollTop);
       done();
+    });
+
+    it('should call scroll up / down while clicking the related node', (done) => {
+      window.scrollTo(0, document.body.clientHeight);
+      console.log(document.body.clientHeight);
+      window.setTimeout(() => {
+        const navItems = document.querySelectorAll('.uxcore-float-nav-item');
+        expect(navItems.length).to.be(16);
+        const expectedResults = [false, false];
+        component.nav.setState = () => {
+          expectedResults[1] = true;
+        }
+        // simulate call the scroller handler
+        component.nav.handleScrollUp({
+          preventDefault: () => {
+            expectedResults[0] = true;
+          }
+        });
+        expect(navItems[0].classList.contains('uxcore-float-nav-item-active')).to.be(true);
+        expect(expectedResults.every(r => !!r)).to.be(true);
+        done();
+      }, 500);
     });
   });
 });
